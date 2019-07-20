bd8c8393d84a919d3568d95345b66bc3d6f43b28
Erik Cribley <erikcribley@gmail.com>
2019-07-20 10:00:17 -0400

    ability score generation functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,30 @@
+// Die Functions
+function dFour () {
+    let roll = Math.floor((Math.random() * 4) + 1)
+    return roll
+}
+
+function dSix () {
+    let roll = Math.floor((Math.random() * 6) + 1)
+    return roll
+}
+
+function dEight () {
+    let roll = Math.floor((Math.random() * 8) + 1)
+    return roll
+}
+
+function dTen () {
+    let roll = Math.floor((Math.random() * 10) + 1)
+    return roll
+}
+
+function dTwelve () {
+    let roll = Math.floor((Math.random() * 12) + 1)
+    return roll
+}
+
+//Score generation functions
 function add (x) {
     let array = x
     let sum = 0
@@ -5,22 +32,49 @@ function add (x) {
         sum += array[i]
     }
     console.log(sum)
+    return sum
 }
 
 function dropLowest (x) {
     x.sort()
     x.shift()
     console.log(x)
-    add(x)
+    return x
 }
 
 function fourDSix () {
     let array = []
     for (i = 0; i < 4; i++) {
-    array.push(Math.floor((Math.random() * 6) + 1))
-    }    
+    array.push(dSix())
+    }
     console.log(array)
-    dropLowest(array)
+    return array
 }
 
-console.log(fourDSix())
+function threeDSix () {
+    let array = []
+    for (i = 0; i < 3; i++) {
+        array.push(dSix())
+    }
+    console.log(array)
+    return array
+}
+
+//Score Generation methods
+function standard () {
+    let abilities = []
+    for (j = 0; j < 6; j++) {
+        abilities.push(add(dropLowest(fourDSix())))
+    }
+    console.log(abilities)
+    return abilities
+}
+
+function classic () {
+    let abilities = []
+    for (j = 0; j < 6; j++) {
+        abilities.push(add(threeDSix()))
+    }
+    console.log(abilities)
+    return(abilities)
+}
