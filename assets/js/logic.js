9409828be5f8ea62d0ed46431731c966a1d5ee59
Erik Cribley <erikcribley@gmail.com>
2019-11-15 01:46:15 -0500

    es6, functions for dis/advantage, rerolling ones

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,45 +1,84 @@
 //dice functions
-function dice (x, y) {
-    let array = []
-    for (i = 0; i < x; i++) {
-        array.push(Math.floor((Math.random() * y) + 1))
-    }
-    console.log(array)
-    return array
+dice = (n, side) => {
+  let array = []
+  for (i = 0; i < n; i++) {
+    array.push(Math.floor((Math.random() * side) + 1))
+  }
+  console.log(array)
+  return array
 }
 
-function dropLowest (x) {
-    x.sort()
-    x.shift()
-    console.log(x)
-    return x
+dropLowest = (arr) => {
+  arr.sort()
+  arr.shift()
+  console.log(arr)
+  return arr
 }
 
-function add (x) {
-    let array = x
-    let sum = 0
-    for (i = 0; i < array.length; i++) {
-        sum += array[i]
+add = (arr) => {
+  let sum = 0
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  console.log(sum)
+  return sum
+}
+
+rerollOnes = (arr) => {
+  for (i = 0; i < arr.length; i++) {
+    while (arr[i] === 1) {
+      arr.splice(i, 1, Math.floor((Math.random() * 6) + 1))
     }
-    console.log(sum)
-    return sum
+  }
+  console.log(arr)
+  return arr
 }
 
 //ability score generation functions
-function standard () {
-    let abilities = []
-    for (j = 0; j < 6; j++) {
-        abilities.push(add(dropLowest(dice(4, 6))))
-    }
-    console.log(abilities)
-    return abilities
+standard = () => {
+  let abilities = []
+  for (j = 0; j < 6; j++) {
+    abilities.push(add(dropLowest(dice(4, 6))))
+  }
+  console.log(abilities)
+  return abilities
 }
 
-function classic () {
-    let abilities = []
-    for (j = 0; j < 6; j++) {
-        abilities.push(add(dice(3, 6)))
-    }
-    console.log(abilities)
-    return(abilities)
+classic = () => {
+  let abilities = []
+  for (j = 0; j < 6; j++) {
+    abilities.push(add(dice(3, 6)))
+  }
+  console.log(abilities)
+  return(abilities)
 }
+
+op = () => {
+  let abilities = []
+  for (j = 0; j < 6; j++) {
+    abilities.push(add(dropLowest(rerollOnes(dice(4, 6)))))
+  }
+  console.log(abilities)
+  return abilities
+}
+
+//advantage & disadvantage
+advantage = () => {
+  let roll = dice(2, 20)
+  if (roll[0] >= roll[1]) {
+    return roll[0]
+  } else {
+    return roll[1]
+  }
+}
+
+disadvantage = () => {
+  let roll = dice(2, 20)
+  if (roll[0] <= roll[1]) {
+    return roll[0]
+  } else {
+    return roll[1]
+  }
+}
+
+console.log(rerollOnes(dice(4, 6)))
